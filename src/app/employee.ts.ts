d90fd67f9c4e2183665941632855622897e99952
Ameen Moosa <32829261+amoosa2020@users.noreply.github.com>
2021-11-22 22:02:02 +0530

    Add files via upload

---
+++ src/app/employee.ts.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    id: number;
+    deptID: number;
+    firstName: string;
+    lastName: string;
+    hireDate: string;
+    emailId: string;
+    managerId: string;
+    phoneNumber: string;
+    salary: number;
+    active: boolean;
+}
